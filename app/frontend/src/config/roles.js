fbff14771089e9cb6b08a752fbc26bd5ea38738e
Varuna2026 <debasish.biswas@varuna.net>
2026-09-07 07:56:53 +0000

    Add P2 role configuration

---
+++ app/frontend/src/config/roles.js
@@ -0,0 +1,15 @@
+export const ROLES = {
+  VENDOR: 'VENDOR',
+  WH: 'WH',
+  SITE_MANAGER: 'SITE_MANAGER',
+  ACCOUNTS: 'ACCOUNTS',
+  ACCOUNTS_PAID: 'ACCOUNTS_PAID',
+}
+
+export const ROLE_LABELS = {
+  VENDOR: 'Vendor',
+  WH: 'Warehouse',
+  SITE_MANAGER: 'Site / WH Manager',
+  ACCOUNTS: 'Accounts',
+  ACCOUNTS_PAID: 'Accounts Paid',
+}
